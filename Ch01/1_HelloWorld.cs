04b01fc5b82beb2aa83d523c6962f1a861230342
Your Name <greenleaf008@naver.com>
2022-06-08 20:08:41 +0900

    2022/06/08 테스트

---
+++ Ch01/1_HelloWorld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch01
+{
+    internal class _1_HelloWorld
+    {
+        static void Main1(string[] args)
+        {
+
+            // 기본출력
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#!");
+
+            // 서식출력
+            Console.Write("Hello\t");
+            Console.Write("korea\n");
+
+            //포맷출력
+            Console.WriteLine("{0}, {1}", "Hello", "Busan");
+
+
+
+
+        }
+    }
+}

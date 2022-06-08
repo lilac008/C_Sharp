db0a9916dda7ef6d847cdf3fb4560c61fa8ad3c6
Your Name <greenleaf008@naver.com>
2022-06-08 20:13:23 +0900

    2022/06/08 수업예제

---
+++ Ch03/1_If.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜 : 2022/06/03
+ * 이름 : 최희원
+ * 내용 : 조건문 If (p127)
+ * 
+ * 
+ */
+
+
+namespace Ch03
+{
+    internal class _1_If
+    {
+        static void Main1(string[] args)
+        {
+            // If
+            int num1 = 1;
+            int num2 = 2;
+
+            if (num1 < num2)
+            { Console.WriteLine("num1은 num2보다 작다."); }
+
+            if (num1 > 1)
+            { Console.WriteLine("num1은 1보다 크다."); }
+
+
+            if (num1 > 0)
+            {
+                if (num2 > 1)
+                { Console.WriteLine("num1은 0보다 크고, num2는 1보다 크다."); }
+
+            }
+
+
+            if (num1 > 0 && num2 > 1)
+            { Console.WriteLine("num1은 0보다 크고 그리고 num2는 1보다 크다."); }
+            Console.WriteLine();
+
+
+
+            // If ~ else
+            int var1 = 1, var2 = 2;
+
+            if (var1 > var2)
+            { Console.WriteLine("var1은 var2보다 크다"); }
+            else
+            { Console.WriteLine("var1은 var2보다 작다."); }
+            Console.WriteLine();
+
+
+
+            // If ~ else if ~ else
+            int n1 = 10, n2 = 20, n3 = 30, n4 = 40;
+
+            if (n1 > n2)
+            { Console.WriteLine("n1은 n2보다 크다."); }
+            else if (n2 > n3)
+            { Console.WriteLine("n2는 n3보다 크다."); }
+            else if (n3 > n4)
+            { Console.WriteLine("n3는 n4보다 크다."); }
+            else
+            { Console.WriteLine("n4가 가장 크다."); }
+            Console.WriteLine();
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Ch03/3_For.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 
+ * 날짜 : 2022/06/07
+ * 이름 : 최희원
+ * 내용 : 반복문 For (P157) 
+ * 
+ * 
+ */
+
+namespace Ch03
+{
+    internal class _3_For
+    {
+        static void Main1(string[] args)
+        {
+            // 반복 for
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("{0}회 반복...", i);
+
+            }
+            Console.WriteLine();
+
+
+            //1부터 10까지 합
+
+            int sum = 0;
+
+            for (int k = 1; k <= 10; k++)
+            {
+                sum += k;       //sum = sum + k;
+            }
+            Console.WriteLine("1부터 10까지 합 : " + sum);
+            Console.WriteLine();
+
+
+            // 1부터 10까지 짝수합
+            int tot = 0;
+
+            for (int k = 1; k <= 10; k++)
+            {
+                if (k % 2 == 0)              //??????????????????
+                {
+                    tot += k;
+                }
+            }
+
+            Console.WriteLine("1부터 10까지 짝수합 : " + tot);
+            Console.WriteLine();
+
+
+            // 중첩 반복 for   (thread라고 함)
+
+            for (int a = 1; a <= 3; a++)
+            {
+                Console.WriteLine("a : " + a);
+
+                for (int b = 1; b <= 4; b++)
+                {
+                    Console.WriteLine("b : " + b);
+
+                    for (int c = 1; c <= 5; c++)
+                    {
+                        Console.WriteLine("c : " + c);
+                    }
+                }
+            }
+            Console.WriteLine();
+
+
+            //구구단
+
+            for (int x = 2; x <= 9; x++)
+            {
+                Console.WriteLine("{0}단 시작", x);
+
+                for (int y = 1; y <= 9; y++)
+                {
+                    int z = x * y;
+                    Console.WriteLine("{0} x {1} = {2}", x, y, z);
+
+                }
+
+
+            }
+            Console.WriteLine();
+
+
+            // 역 별삼각형
+            for (int start = 1; start <= 10; start++)
+            {
+                for (int end = 10; start <= end; end--)
+                {
+                    Console.Write("★");
+                }
+                Console.WriteLine(); // 줄바꿈
+            }
+            Console.WriteLine();
+
+
+            // 별삼각형
+           
+
+
+
+
+
+        }
+    }
+}

f2f478794418d170b3f2c53ee433d01a1ca25144
Your Name <greenleaf008@naver.com>
2022-06-09 21:37:51 +0900

    2022/06/09 수업예제

---
+++ Ch05/1_Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch05
+{
+    internal class _1_Class
+    {
+        static void Main1(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
